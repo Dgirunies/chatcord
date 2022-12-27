822eba3e50a4a11f0688db93e9f751d2d99dda7e
sgednethgirao <edneth@sheetgo.com>
2022-12-27 22:51:19 +0100

    fix: logging object messages

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 const PORT = 3000 || process.env.PORT
+const botName = 'ChatcordBot'
 
 const app = express()
 const server = http.createServer(app)
@@ -19,19 +21,19 @@ io.on('connection', socket => {
     // io.emit() => Sends a message to all connected clients including the current one 
     
     // Welcome current user
-    socket.emit('message', 'Welcome to Chatcord!')
+    socket.emit('message', formatMessage(botName, 'Welcome to Chatcord!'))
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat')
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
 
     // Runs when client diconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName, 'A user has left the chat'))
     })
 
     // Listen to chat message
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg)
+        io.emit('message', formatMessage('USER', msg))
     })
 })
 
